63dd16e99207474111b75fa73ddbe492b1b95727
kinhopoa <marcosfraga05@gmail.com>
2022-05-31 21:10:17 -0300

    alteração de diagrama de classe vacina e criacao da classe vacina

---
+++ vacina.py
@@ -0,0 +1,15 @@
+class vacina:
+    def __init__(self, id_vacina, nomeVacina, tipo, dose, disponibilidade):
+        self._id_vacina = id_vacina
+        self._nomeVacina = nomeVacina;
+        self._tipo = tipo;
+        self._dose = dose;
+    
+    @property
+    def get_nome_vacina(self):
+        return self._nomeVacina = nomeVacina
+
+    @property
+    def set_nome_vacina(self):
+        self._nomeVacina = nomeVacina
+    
