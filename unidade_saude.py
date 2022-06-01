5b4ebc5e129ba55fd8132dcdee22d15f5da3e8f0
ThonyFs <thonyfelipesilva@gmail.com>
2022-05-31 21:35:19 -0300

    criacao da classe UnidadeSaude

---
+++ unidade_saude.py
@@ -0,0 +1,7 @@
+class UnidadeSaude():
+    def __init__(self, nome, endereco, telefone, cnpj):
+        self._nome = nome
+        self._endereco = endereco
+        self._telefone = telefone
+        self._cnpj = cnpj
+        
