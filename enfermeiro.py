b12b2276e703120571af8545457d2d19fe527375
kinhopoa <marcosfraga05@gmail.com>
2022-06-07 13:38:34 -0300

    criação de metodo get na classe enfermeiro

--- enfermeiro.py
+++ enfermeiro.py
@@ -5,4 +5,9 @@ class Enfermeiro(Pessoa):
         super().__init__(nome, idade, telefone)
         self._registro = registro
 
+    def get_registro(self):
+        return self._registro
+
+
 enf= Enfermeiro(102, "thony", 28, "980546478")
+print(enf.get_registro())
