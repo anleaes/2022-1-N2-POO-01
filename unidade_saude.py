2ab8feba131a23d4040682e79604dfa72d003a2c
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-07 09:58:55 -0300

    impementacao de objeto na classe UnidadeSaude

--- unidade_saude.py
+++ unidade_saude.py
@@ -4,4 +4,5 @@ class UnidadeSaude():
         self._endereco = endereco
         self._telefone = telefone
         self._cnpj = cnpj
-        
+
+unidade1 = UnidadeSaude("Posto Modelo", "Av. Joao Pessoa", "32175004", "93.044.485-0001")
