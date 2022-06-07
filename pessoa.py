36110662c05257a6cd4c04c2183eaf9db14bcd2a
kinhopoa <marcosfraga05@gmail.com>
2022-06-07 14:34:39 -0300

    criação de método get_nome na classe Pessoa

--- pessoa.py
+++ pessoa.py
@@ -4,4 +4,8 @@ class Pessoa:
         self._idade = idade
         self._telefone = telefone
 
+    def get_nome(self):
+        return self._nome
+
 p1 = Pessoa("Marco", 25, "980465784")
+print(p1.get_nome())
