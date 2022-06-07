265290721acd6aa4afff2ca24eb52cea8370eba2
kinhopoa <marcosfraga05@gmail.com>
2022-06-07 14:51:46 -0300

    criação do metodo set_nome na classe Pessoa

--- pessoa.py
+++ pessoa.py
@@ -13,8 +13,17 @@ class Pessoa:
     def get_telefone(self):
         return self._telefone
 
+    def set_nome(self, nome):
+        self._nome = nome
+
 p1 = Pessoa("Marco", 25, "980465784")
 print(p1)
 print(p1.get_nome())
 print(p1.get_idade())
 print(p1.get_telefone())
+
+print('------------------------')
+print('')
+
+p1.set_nome('Thony')
+print(p1.get_nome())
