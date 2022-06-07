9ebb3f8efc162a23bef918a9ff64cc689339898e
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-07 08:26:21 -0300

    correcao naa classe pessoa

--- cadastroPaciente.py
+++ cadastroPaciente.py
@@ -2,13 +2,13 @@ from pessoa import Pessoa
 
 class CadastroPaciente(Pessoa):
         def __init__(self, nome, idade, telefone, cpf, email, endereco):
-            super.()__init__(nome, idade, telefone):
+            super().__init__(nome, idade, telefone)
                 self._cpf = cpf
                 self._email = email
                 self._endereco = endereco
 
-        def validarCadastro(self):
-            if self._cpf === NULL and self._pessoa === NULL:
-                print('Falha no cadastro. Por favor preencha o CPF!')
+        def validarCadastro(self, cpf, ):  
+            if self._cpf == NULL and self._pessoa == NULL:
+                print("Falha no cadastro. Por favor preencha o CPF!")
             else:
-                print('Cadastro realizado com sucesso')
+                print("Cadastro realizado com sucesso")
