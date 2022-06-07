dc13016cabfde9e5c2cf9708bd28e5be24daed75
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-07 10:27:28 -0300

    implementacao de objeto enf na classe Enfermeiros

--- enfermeiro.py
+++ enfermeiro.py
@@ -1,8 +1,8 @@
 from pessoa import Pessoa
 
 class Enfermeiro(Pessoa):
-    def __init___(self, registro, nome, idade, telefone):
+    def __init__(self, registro, nome, idade, telefone):
         super().__init__(nome, idade, telefone)
         self._registro = registro
 
-    
+enf= Enfermeiro(102, "thony", 28, "980546478")
