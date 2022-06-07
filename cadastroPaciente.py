46d6f3ecf7260fe17d0d6c6d4e55eb4efe925792
kinhopoa <marcosfraga05@gmail.com>
2022-06-07 14:07:34 -0300

    criação de método get_endereco na classe CadastroPaciente

--- cadastroPaciente.py
+++ cadastroPaciente.py
@@ -16,7 +16,11 @@ class CadastroPaciente(Pessoa):
     def get_email(self):
         return self._email
 
+    def get_endereco(self):
+        return self._endereco
 
-pc = CadastroPaciente(1234, "poa, ", "teste@teste.com", "thony", 28, "980631415")
+
+pc = CadastroPaciente(1234, "Rua tal, 122 ", "teste@teste.com", "thony", 28, "980631415")
 print(pc)
 print(pc.get_email())
+print(pc.get_endereco())
