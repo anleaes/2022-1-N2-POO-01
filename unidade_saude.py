1b4f58b705d22e2f058be21af7a8b4b73aa977a5
kinhopoa <marcosfraga05@gmail.com>
2022-06-07 16:58:32 -0300

    criação de método get_cnpj na classe UnidadeSaude

--- unidade_saude.py
+++ unidade_saude.py
@@ -14,5 +14,8 @@ class UnidadeSaude():
     def get_telefone(self):
         return self._telefone
 
+    def get_cnpj(self):
+        return self._cnpj
+
 unidade1 = UnidadeSaude("Posto Modelo", "Av. Joao Pessoa", "32175004", "93.044.485-0001")
 
