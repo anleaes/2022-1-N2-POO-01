c0c13bd5fbbf6722600c66694a84a70ec65668fc
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-07 20:27:47 -0300

    atualizacao da classe

--- marcacao_vacina.py
+++ marcacao_vacina.py
@@ -1,16 +1,10 @@
 from cadastroPaciente import CadastroPaciente
 from agenda import Agenda
-from vacina import Vacina
 
-class MarcacaoVacina(CadastroPaciente, Agenda, Vacina):
-    def __init__(self, idMarcacao, cadastroPaciente, agenda):
-        CadastroPaciente.__init__(self, cpf, email, endereco, pessoa)
-        Agenda.__init__(self, hora, sala, Vacina)
-        Vacina.__init__(self, id_vacina,nomeVacina,tipo, dose, disponibilidade, Agenda)
-        self._idMarcacao = idMarcacao
+class MarcacaoVacina(CadastroPaciente, Agenda):
+    def __init__(self, idMarcacao):
+       self._idMarcacao = idMarcacao
 
     def get_id(self):
         return self._idMarcacao
 
-    def set_id(self, idMarcacao):
-        self._idMarcacao = idMarcacao
