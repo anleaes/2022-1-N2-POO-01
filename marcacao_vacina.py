e02a016ea1c3c70071c836c244f9aa35fc08413f
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-07 21:13:37 -0300

    correcao na classe cmarcacaoVacina

--- marcacao_vacina.py
+++ marcacao_vacina.py
@@ -1,10 +1,14 @@
 from cadastroPaciente import CadastroPaciente
 from agenda import Agenda
 
-class MarcacaoVacina(CadastroPaciente, Agenda):
-    def __init__(self, idMarcacao):
+class MarcacaoVacina:
+    def __init__(self, idMarcacao, cadastro_paciente, agenda):
        self._idMarcacao = idMarcacao
+       self._cadastro_paciente = cadastro_paciente
+       self._agenda = agenda
 
     def get_id(self):
         return self._idMarcacao
+
+    
 
