1e22f1608f754b7d12c9dbed480845b50fe05b59
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-08 17:12:17 -0300

    alteracoes

--- agenda.py
+++ agenda.py
@@ -10,7 +10,7 @@ class Agenda:
         
     @property
     def get_informar_horario(self):
-        return print(f" Hora: {self._hora}")
+        return print(f"Hora: {self._hora}")
 
     @get_informar_horario.setter
     def set_horario(self, hora):
@@ -18,7 +18,7 @@ class Agenda:
         
     @property
     def get_informar_data(self):
-        return print(f" Hora: {self._data}")
+        return print(f"Data: {self._data}")
 
     @get_informar_horario.setter
     def set_data(self, data):
@@ -29,11 +29,10 @@ sala1 = Sala(10, 2, "unidade1")
 vacina1 = Vacina(180, "Clozapina", "Viral", "dose unica", True)
 agenda1 = Agenda("10:30", "08/06/2022", sala1, vacina1)
 
-agenda1.get_informar_data
-agenda1.get_informar_horario
-agenda1._sala.get_andar
-agenda1._sala.get_numero_sala
-agenda1._vacina.get_nome_vacina
-agenda1._vacina.get_tipo
-agenda1._vacina.get_dose
-agenda1._vacina.get_disponibilidade
+# agenda1.get_informar_data
+# agenda1.get_informar_horario
+# agenda1._sala.get_andar
+# agenda1._sala.get_numero_sala
+# agenda1._vacina.get_nome_vacina
+# agenda1._vacina.get_tipo
+# agenda1._vacina.get_dose
