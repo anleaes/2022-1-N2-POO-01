c30fd851837fe2fe5b3107b900cdecf54cc2fc96
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-08 15:57:11 -0300

    alteracao de metodos

--- agenda.py
+++ agenda.py
@@ -1,6 +1,5 @@
 from sala import Sala
 from vacina import Vacina
-from unidade_saude import UnidadeSaude
 
 class Agenda():
     def __init__(self, hora, data, sala, vacina):
@@ -10,28 +9,28 @@ class Agenda():
         self._vacina = vacina
         
     @property
-    def get_verificar_horario(self):
-        return self._hora
+    def get_informar_horario(self):
+        return print(f" Hora: {self._hora}")
 
-    @get_verificar_horario.setter
+    @get_informar_horario.setter
     def set_horario(self, hora):
         self._hora = hora
-
+        
     @property
-    def get_verificar_sala(self):
-        return self._sala
+    def get_informar_data(self):
+        return print(f" Hora: {self._data}")
 
-    @get_verificar_sala.setter
-    def set_sala(self, sala):
-        self._sala = sala
-
+    @get_informar_horario.setter
+    def set_data(self, data):
+        self._data = data
+   
     def agendar_vacina(self):
         if self._vacina is not False:
             return print(f'Vacina {self._vacina} disponivel para o dia ...')
         else:
             return print(f'A vacina não está disponível no momento.')
 
-
-
-
+sala1 = Sala(10, 2, "unidade1")
+vacina1 = Vacina(180, "Clozapina", "viral", "dose unica", True)
+agenda1 = Agenda("10:30", "08/06", sala1, vacina1)
 
