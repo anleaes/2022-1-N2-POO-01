2bb52ca15323cbb8aa636d11dae79ce24046a3cc
Kinhopoa <marcosfraga05@gmail.com>
2022-06-07 21:59:53 -0300

    Alteração nos métodos getter da classe MarcacaoVacina

--- marcacao_vacina.py
+++ marcacao_vacina.py
@@ -5,6 +5,7 @@ class MarcacaoVacina(CadastroPaciente, Agenda):
     def __init__(self, idMarcacao):
        self._idMarcacao = idMarcacao
 
+    @property
     def get_id(self):
         return self._idMarcacao
 
