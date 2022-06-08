448d96782c54aa2e425dc08757415488a0cb85a0
Kinhopoa <marcosfraga05@gmail.com>
2022-06-07 21:57:23 -0300

    alteração nos métodos getters e setters da classe Pessoa

--- pessoa.py
+++ pessoa.py
@@ -4,22 +4,29 @@ class Pessoa:
         self._idade = idade
         self._telefone = telefone
 
+    @property
     def get_nome(self):
         return self._nome
 
-    def get_idade(self):
-        return self._idade
-    
-    def get_telefone(self):
-        return self._telefone
-
+    @get_nome.setter
     def set_nome(self, nome):
         self._nome = nome
 
+    @property
+    def get_idade(self):
+        return self._idade
+
+    @get_idade.setter
     def set_idade(self, idade):
         self._idade = idade
 
+    @property
+    def get_telefone(self):
+        return self._telefone
+
+    @get_telefone.setter
     def set_telefone(self, telefone):
         self._telefone = telefone
 
+    
 p1 = Pessoa("Marco", 25, "980465784")
