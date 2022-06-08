a2f1e14a3b61cdbb2ad799a21983a5071a37cb24
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-08 15:15:01 -0300

    alteracoes nas classe visando metodos

--- marcacao_vacina.py
+++ marcacao_vacina.py
@@ -15,10 +15,22 @@ class MarcacaoVacina:
     def get_id(self):
         return self._idMarcacao
 
-
+    def validar_paciente(self, cadastro_paciente):
+        if self._cadastro_paciente == cadastro_paciente:
+            print("Validação efetuada!")
+        else:
+            print("Dados incompativeis")
+        
+#Objetos instanciados para execução do codigo
 cPaciente =  CadastroPaciente("03232588409", "Poa", "teste@teste.com", "Josiclei Judison", 30, "32178009")
 enf= Enfermeiro(102, "thony", 28, "980546478")
 unidade1 = UnidadeSaude("Posto Modelo", "Av. Joao Pessoa", "32175004", "93.044.485-0001")
 sala1 = Sala(10, 2, unidade1)
 vacina1 = Vacina(180, "Clozapina", "viral", "dose unica", True)
 agenda1 = Agenda("10:30", sala1, vacina1)
+Mconsulta = MarcacaoVacina(10, cPaciente, agenda1, enf)
+
+#execução dos metodos
+Mconsulta.validar_paciente(cPaciente)
+print(Mconsulta._agenda._sala.get_numero_sala)
+
