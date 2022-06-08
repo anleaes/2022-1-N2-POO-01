d30d6fea0814ef615409eec6cf0312f215b2266f
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-08 13:07:29 -0300

    objeto instaciando classe CadastroPaciente

--- marcacao_vacina.py
+++ marcacao_vacina.py
@@ -11,12 +11,12 @@ class MarcacaoVacina:
        self._cadastro_paciente = cadastro_paciente
        self._agenda = agenda
        self._enfermeiro = enfermeiro
-       
+
 
     def get_id(self):
         return self._idMarcacao
 
-   
+cPaciente =  CadastroPaciente("03232588409", "Poa", "teste@teste.com", "Josiclei Judison", 30, "32178009")
 enf= Enfermeiro(102, "thony", 28, "980546478")
 unidade1 = UnidadeSaude("Posto Modelo", "Av. Joao Pessoa", "32175004", "93.044.485-0001")
 sala1 = Sala(10, 2, unidade1)
