6d05c57b68fce0afe68be5b10e4c0883e8605089
ThonyFs <thonyfelipesilva@gmail.com>
2022-06-08 15:58:19 -0300

    ajuste final

--- agenda.py
+++ agenda.py
@@ -24,13 +24,6 @@ class Agenda():
     def set_data(self, data):
         self._data = data
    
-    def agendar_vacina(self):
-        if self._vacina is not False:
-            return print(f'Vacina {self._vacina} disponivel para o dia ...')
-        else:
-            return print(f'A vacina não está disponível no momento.')
+   
 
-sala1 = Sala(10, 2, "unidade1")
-vacina1 = Vacina(180, "Clozapina", "viral", "dose unica", True)
-agenda1 = Agenda("10:30", "08/06", sala1, vacina1)
 
